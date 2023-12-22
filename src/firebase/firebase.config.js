a277268da48be44f9daa071434ed59f5880c1097
Hossam Hatem <7ossam7atem1@gmail.com>
2023-12-22 15:14:04 +0200

    React Web App

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDn_PnuI21vGjSppxd2aZVkFeWDdxRMeXQ",
+  authDomain: "ecommerce-react-js-58ca6.firebaseapp.com",
+  projectId: "ecommerce-react-js-58ca6",
+  storageBucket: "ecommerce-react-js-58ca6.appspot.com",
+  messagingSenderId: "2630826977",
+  appId: "1:2630826977:web:75f9f62a634fef48ea70a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
